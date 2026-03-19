3744c8e8333128f88461e73b750c4541beaaab40
IANHELL <angelcastillougena@gmail.com>
2026-03-19 22:47:40 +0100

    Add files via upload

--- api/heygen-asset.js
+++ api/heygen-asset.js
@@ -1,3 +1,9 @@
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Api-Key, x-api-key, X-File-Type');
